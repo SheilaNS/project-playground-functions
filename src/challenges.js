5fb00bf576891e51eb6634636107fec86ec4ee3d
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2021-12-01 11:30:32 -0300

    Initial commit

---
+++ src/challenges.js
@@ -0,0 +1,60 @@
+// Desafio 1
+function compareTrue() {
+  // seu código aqui
+}
+
+// Desafio 2
+function calcArea() {
+  // seu código aqui
+}
+
+// Desafio 3
+function splitSentence() {
+  // seu código aqui
+}
+
+// Desafio 4
+function concatName() {
+  // seu código aqui
+}
+
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
+
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
+
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
+
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
+
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  highestCount,
+  splitSentence,
+};
---
+++ src/challenges2.js
@@ -0,0 +1,26 @@
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+module.exports = {
+  generatePhoneNumber,
+  techList,
+  hydrate,
+  triangleCheck,
+};
