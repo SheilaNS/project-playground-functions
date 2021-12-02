4bb93c48c92744cbfb4048fc72750ce597e4f084
SheilaNS <shei.nsantos@gmail.com>
2021-12-02 15:12:57 -0300

    Requisito 1 ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
